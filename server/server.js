2762d8e5f74060d375c835add15f6d87b4b6a3ac
robester0403 <robkso@gmail.com>
2022-10-05 18:08:18 -0400

    added connection to mongodb with mongoose without routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,36 @@
-const cors = require("cors");
-const express = require("express");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+const cors = require("cors"),
+  express = require("express"),
+  mongoose = require("mongoose"),
+  morgan = require("morgan"),
+  cookieParser = require("cookie-parser"),
+  bodyParser = require("body-parser");
 
 const app = express();
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }) //is there better way to display this console log?
+  .then(() =>
+    console.log("MongoDB database connection established successfully")
+  )
+  .catch((err) => console.log("Error: ", err));
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(morgan("combined"));
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+// listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
