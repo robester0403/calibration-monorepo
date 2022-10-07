b1a178413369708466cb1281c3bd1462ae46ddae
robester0403 <robkso@gmail.com>
2022-10-07 09:31:33 -0400

    Updated Frontend with initial fetch all

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import isEmpty from "lodash/isEmpty";
 
-function App() {
+const App = () => {
+  const [allProjects, setAllProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null); // see how we want to handle errors
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:8079/api/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Fetch went wrong");
+        }
+      })
+      .then(({ projects }) => {
+        setAllProjects(projects);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+  console.log(allProjects);
+
+  if (!isEmpty(error)) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <h1>Loading...</h1>}
+      {!loading && !error && allProjects.length > 0 && (
+        <ul>
+          {allProjects.map(({ name, _id, description, techStackTags }) => (
+            <li key={_id}>
+              {name}, {description}, {techStackTags}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default App;
